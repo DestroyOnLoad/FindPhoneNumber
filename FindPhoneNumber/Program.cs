ab6643ed087c4d2938ceaf427f6c1c0f058a31c9
David Potter <destroyonload@gmail.com>
2020-12-08 13:49:23 +0800

    Added method that finds the phone number in a string. Added helper method that removes all whitespace from the string that phone number might be found in. Test with blob string worked successfully. Starting to work on the function to read string values from a CSV file.

--- FindPhoneNumber/Program.cs
+++ FindPhoneNumber/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace FindPhoneNumber
 {
     class Program
     {
+        static readonly string readablePath = @"C:\Users\PCMiner1\Desktop\Dietary Supplements\All Products\id-url_Lookup.csv";
+        static string writablePath = @"C:\Users\PCMiner1\Desktop\Dietary Supplements\All Products\phoneList.csv";
+
+        static Regex phoneNumRg = new Regex(@"\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})");
+
+        //temp variables
+        static List<string> values = new List<string>();  //will store full strings read and be rewritten with phone number value;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //take in a list of strings possibly containing phone numbers from csv file
+            using (StreamReader sr = File.OpenText(readablePath))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            //replace each element in the list with the result of find phone number
+
+            //export the list to a new csv file [or new row in original csv file];
+        }
+
+        static string FindPhoneNumber(string s)
+        {
+            if (String.IsNullOrWhiteSpace(s)) return "";
+
+            Match result = phoneNumRg.Match(s.Contains(" ") ? RemoveWhiteSpace(s): s);
+
+            return result.Value;
+        }
+
+        static string RemoveWhiteSpace(string s)
+        {
+            return Regex.Replace(s, @"\s+", "");
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+Program takes in string values from a CSV file, extracts a phone-number if it exists, and writes the results to a new CSV file.
+
+Credit for REGEX structure of a phone number goes to gadildafissh on StackOverflow.  Contributor's original code can be found at:
+https://stackoverflow.com/questions/8596088/c-sharp-regex-phone-number-check.
