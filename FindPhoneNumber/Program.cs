c6453874ffc4fd36c29596a2d9b49033cfb1c1fa
David Potter <destroyonload@gmail.com>
2020-12-08 12:16:55 +0800

    init commit

---
+++ FindPhoneNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
